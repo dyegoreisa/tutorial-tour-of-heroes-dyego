e7e8befe06316ac113121b58beb38ab4204054a7
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2016-10-12 18:38:12 -0300

    Select a Hero in the HeroesComponent

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Location } from '@angular/common';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  constructor( private location: Location ) { }
+
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
