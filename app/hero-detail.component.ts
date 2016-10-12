deb54fa9ae83e011b87fcf05c05b98503b4d2149
Dyego Reis de Azevedo <dyegoreisa@gmail.com>
2016-10-12 18:56:48 -0300

    Styling the App

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -8,17 +8,20 @@ import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
-  templateUrl: 'app/hero-detail.component.html'
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: [ 'app/hero-detail.component.css' ]
 })
+
 export class HeroDetailComponent implements OnInit {
-  @Input()
   hero: Hero;
+
   ngOnInit(): void {
     this.route.params.forEach((params: Params) => {
       let id = +params['id'];
       this.heroService.getHero(id).then(hero => this.hero = hero);
     });
   }
+
   constructor(
       private heroService: HeroService,
       private route: ActivatedRoute,
